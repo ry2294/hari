d8e6b783b27e78e0c565011322999d24a7eaf3f4
Rakesh Yarlagadda <ry2294@columbia.edu>
2017-07-22 23:13:15 -0700

    Final Server changes

--- server.js
+++ server.js
@@ -7,14 +7,27 @@ to the client browser.
 */
 var app = express();
 var router = express.Router();
+var request = require('request');
+
 router.get('/', function(req, res) {
   res.sendfile(path.join(__dirname + '/home.html'));
 });
 
 router.get('/search', function(req, res) {
     console.log(req.query.text);
-    var words = ["wedding word", "wedding date", "wedding anniversary"];
-    return res.status(200).send(JSON.stringify(words)).end();
+    
+    request('http://35.184.84.199:9200/bestbuy/products/_search?q=' + 
+            req.query.text, 
+            function (error, response, body) {
+              var data = JSON.parse(body);
+              var words = [];
+              if (data != null && data.hits != null && data.hits.hits != null) {
+                for (var i = 0; i < data.hits.hits.length; i++) {
+                  words.push(data.hits.hits[i]._source.name);
+                }
+              }
+              return res.status(200).send(JSON.stringify(words)).end();
+            });
 });
 
 /*
